afd3c1301be9165f5ebe7dbc90e23b5c41958a8f
Abhinandan Sethi <abhinandansethi07@gmail.com>
2026-02-12 03:31:12 +0530

    render live added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
     try {
       setLoading(true);
 
-      const res = await fetch("http://127.0.0.1:8000/api/match", {
+      const res = await fetch("https://resumatch-5ssz.onrender.com/api/match", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
